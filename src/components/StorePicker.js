8fd39aeecbf8106f9f00cc08f0719a5e88de4574
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2022-02-02 22:04:25 +0530

    Refactor: Update formatting

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -6,7 +6,14 @@ class StorePicker extends React.Component {
         return (
             <form className="store-selector">
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+
+                <input
+                    type="text"
+                    required
+                    placeholder="Store Name"
+                    defaultValue={getFunName()}
+                />
+
                 <button type='submit'>Visit Store →</button>
             </form>
         );
