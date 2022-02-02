f3408d3335007354a48d94375a2622ecaf694542
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2022-02-02 23:03:10 +0530

    Feature: Prevent page reload on form submit

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    storeNameInput = React.createRef();
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
 
                 <input
                     type="text"
+                    ref={this.storeNameInput}
                     required
                     placeholder="Store Name"
                     defaultValue={getFunName()}
@@ -17,6 +20,15 @@ class StorePicker extends React.Component {
                 <button type='submit'>Visit Store →</button>
             </form>
         );
+    }
+
+    goToStore(event) {
+        // Stop the form from reloading the page on submit.
+        event.preventDefault();
+
+        // Get the store name from input.
+
+        // Go to the store page - store/store-name
     }
 }
 
