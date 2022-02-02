f9ba3b2e8bdd55b3a24322c9e0f9a5c4f86dcaca
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2022-02-02 10:48:40 +0530

    Feature: Set route for the application, set route for not found, add NotFound component

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import StorePicker from "./StorePicker";
+import App from "./App";
+import NotFound from "./NotFound";
 
 // Router is a Stateless Functional Component
 const Router = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={StorePicker} />
+            <Route path="/store/:storeId" component={App} />
+            <Route component={NotFound} />
         </Switch>
     </BrowserRouter>
 );
