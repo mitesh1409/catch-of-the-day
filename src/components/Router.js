5385ef52203d2d07d3b7ed2b8b84126b9355727f
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2022-02-02 10:38:10 +0530

    Feature: Add a comment

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import StorePicker from "./StorePicker";
 
+// Router is a Stateless Functional Component
 const Router = () => (
     <BrowserRouter>
         <Switch>
