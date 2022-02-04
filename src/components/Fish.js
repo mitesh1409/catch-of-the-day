e9d24cb9709ff250876fc80330c7f666ca187981
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2022-02-04 23:40:40 +0530

    Feature: Put a check - if the fish is available or sold out

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -5,15 +5,20 @@ class Fish extends React.Component {
     render() {
         const { image, name, desc, price, status } = this.props.details;
 
+        const isAvailable = (status === 'available');
+
         return (
             <li className='menu-fish'>
                 <img src={image} alt={name}/>
+
                 <h3 className='fish-name'>
                     {name}
                     <span className='price'>{formatPrice(price)}</span>
                 </h3>
+
                 <p>{desc}</p>
-                <button>Add To Cart</button>
+
+                <button disabled={!isAvailable}>{isAvailable ? 'Add To Order' : 'Sold Out!'}</button>
             </li>
         );
     }
