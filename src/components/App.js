5b1e37da692a07968da6695e7e60c04af61ddd88
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2022-02-08 13:45:04 +0530

    Feature: Save/retrieve order data to/from local storage

--- src/components/App.js
+++ src/components/App.js
@@ -19,10 +19,21 @@ class App extends React.Component {
 
     componentDidMount() {
         const params = this.props.match.params;
+
+        // First reinstate our local storage. Set order state from the local storage.
+        const localStorageRef = localStorage.getItem(params.storeId);
+        if (localStorageRef) {
+            this.setState({ order: JSON.parse(localStorageRef) });
+        }
+
         this.ref = rebase.syncState(`${params.storeId}/fishes`, {
             context: this,
             state: 'fishes'
         });
+    }
+
+    componentDidUpdate() {
+        localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order));
     }
 
     componentWillUnmount() {
